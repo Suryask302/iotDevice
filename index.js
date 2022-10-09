89767b3cd0f25db0786b9160e88d6d5841773452
Suryask302 <suryask302@gmail.com>
2022-10-09 22:34:46 +0530

    facebookLogin

--- index.js
+++ index.js
@@ -3,18 +3,33 @@ const express = require("express")
 const app = express()
 const port = 3000
 const morgan = require('morgan')
+const passport = require('passport')
+const cookieSession = require('cookie-session')
+require('./utils/passport-setup')
 
-require('./db/connect')
+// require('./db/connect')
 const { notFound, errorHandler } = require('./utils/errors')
-const route = require('./routes/userRoutes')
+// const route = require('./routes/userRoutes')
+const authRoute = require('./routes/auth')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+app.use(
+	cookieSession({
+		name: "session",
+		keys: ["suryasK"],
+		maxAge: 24 * 60 * 60 * 100,
+	})
+)
 
-app.use('/', route)
+app.use(passport.initialize());
+app.use(passport.session());
 
+
+// app.use('/', route)
+app.use('/', authRoute)
 app.use(notFound)
 app.use(errorHandler)
 
